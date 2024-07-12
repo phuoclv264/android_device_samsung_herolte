4d14e60ef28e4f3c8c6785e3a22e4f6761d49aff
phuoclv264 <phuoclv264@gmail.com>
2024-07-12 09:05:26 +0700

    Update init_herolte.cpp

--- init/init_herolte.cpp
+++ init/init_herolte.cpp
@@ -90,7 +90,6 @@ void vendor_load_properties()
     property_override("ro.secure", "0");
     property_override("ro.debuggable", "0");
     property_override("persist.sys.usb.config", "adb");
-    property_override("persist.adb.tcp.port", "5555");
 
     // Turn off audio
     property_override("ro.config.media_vol_default", "0");
